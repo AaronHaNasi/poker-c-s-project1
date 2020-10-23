83bed1a32794dc5356c748bbe660570b36bbf060
AaronIlMentor <kingjojo3@gmail.com>
2020-10-23 09:20:47 -0400

    main.py skeleton classes set up

---
+++ main.py
@@ -0,0 +1,25 @@
+import deck.py
+import hand.py 
+
+def is_fluah(hand):
+    return
+
+def is_straight(): 
+    return
+
+def is_4_of_kind(): 
+    return
+
+def is_full_house():
+    return
+
+def is_3_of_kind(): 
+    return
+
+def is_2_of_kind(): 
+    return
+
+
+
+# Sources:
+# http://www.mathcs.emory.edu/~cheung/Courses/170/Syllabus/10/pokerCheck.html 
